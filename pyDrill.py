1000250b22091080e066d58d042a3e70e140a9cf
Ihsan Muhammad Aulia <ihsanauliaa@gmail.com>
2020-12-20 14:56:16 +0700

    Add hdfs config

--- pyDrill.py
+++ pyDrill.py
@@ -1,19 +1,25 @@
 from pydrill.client import PyDrill
+from pydrill.exceptions import ImproperlyConfigured
+import json
+from datetime import datetime, timedelta
 
-drill = PyDrill(host='localhost', port=8047)
-
-if not drill.is_active():
-    raise ImproperlyConfigured('Please run Drill first')
-
-def test_query() :
-  result = drill.query('''
-    SELECT * FROM api.covid.`daily/12-5-2020`
-  ''')
-      
-  return result
+drill = PyDrill(host='139.59.108.226', port=8047)
+yesterday = datetime.now() - timedelta(1)
+yesterday_string = datetime.strftime(yesterday, '%Y-%m-%d')
+print(yesterday_string)
 
 
-# pandas dataframe
+def test_query():
+    # drill.is_active()
+    if not drill.is_active():
+        raise ImproperlyConfigured('Please run Drill first')
 
-# df = yelp_reviews.to_dataframe()
-# print(df[df['stars'] > 3])
+    query = "select t.`date`, t.iso_code, t.location, t.total_cases, h.Population from " \
+            "hdfs.root.`owid-covid-data.csv` AS t INNER JOIN `hdfs.root`.`population_by_country.csv` AS h ON " \
+            "h.Country = t.location where " \
+            "t.location='Indonesia' AND t.`date`='" + yesterday_string + "' "
+    country = drill.query(query)
+    df = country.to_dataframe()
+    df = json.loads(df.to_json(orient='table'))
+
+    return df
