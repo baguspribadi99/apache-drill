3528ee7851217569fbd30717f2d04070f7c5189b
BagusPrib <bpibadi99@yahoo.co.id>
2020-12-22 16:28:00 +0700

    finish api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from pyDrill import *
 
 app = Flask(__name__)
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return get_all_countries_query()
+
+@app.route("/country")
+def detail():
+    return get_detail_country_query(request.args.get("country"))
